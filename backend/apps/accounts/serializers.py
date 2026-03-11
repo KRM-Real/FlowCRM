191c66fe06aa7b8274926a69482905d52c2d670c
KRM-Real <thisismarkreal@gmail.com>
2026-03-12 01:31:48 +0800

    Sprint 2: Auth - add authenticated /me endpoint

---
+++ backend/apps/accounts/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import User
+
+class MeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", 
+                  "email", 
+                  "first_name", 
+                  "last_name", 
+                  "is_active"
+                ]
